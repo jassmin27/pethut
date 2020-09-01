93dd9c1d7bcd70eec53ed4bdf9eadf1953c384e7
kaurjasmine <a00268652@student.ait.ie>
2020-09-01 02:08:53 +0100

    add pet redirect

--- client/src/components/pet/add-pet.component.js
+++ client/src/components/pet/add-pet.component.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import {Redirect} from "react-router-dom";
 
 class AddPet extends Component {
     constructor(props) {
@@ -13,6 +14,7 @@ class AddPet extends Component {
         this.onChangeDescription = this.onChangeDescription.bind(this);
         this.onChangeImage = this.onChangeImage.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+        this.onAddComplete = this.onAddComplete.bind(this);
 
         this.state = {
           name: '',
@@ -23,7 +25,8 @@ class AddPet extends Component {
           description: '',
           image: '',
           formSuccess: false,
-          errors: []
+          errors: [],
+          redirect: false
         }
     }
     onChangeName(e) {
@@ -61,6 +64,12 @@ class AddPet extends Component {
           image: e.target.files[0]
         })
     }
+    onAddComplete() {
+        this.setState({
+          redirect: true
+        })
+    }
+
     onSubmit(e) {
         e.preventDefault();
 
@@ -87,83 +96,89 @@ class AddPet extends Component {
           .then(res => {
             console.log(res.data);
             this.setState({
-              formSuccess: true
+              formSuccess: true,
+              redirect: true
             })
-            setTimeout(() => {  window.location = '/owners/' + this.props.match.params.owner_id; }, 2000);
+            setTimeout(() => { this.onAddComplete(); }, 1500);
 
           })
           .catch( err => {
               console.log(err.response.data);
               this.setState({
                 errors: err.response.data.errors,
-                formSuccess: false
+                formSuccess: false,
+                redirect: false
               })
             });
     }
 
     render() {
-        return(
-            <div className="my-4 div-width">
-                {this.state.formSuccess === true && (
-                    <div className="success-msg mb-3">
-                        <p className="m-0 p-0">Pet Added Successfully!</p>
-                    </div>
-                 )}
-                <div className="mt-2 mb-4"><h2>Add Pet</h2></div>
-                <form className="pet-details" onSubmit = {this.onSubmit}>
-                    <div className="form-group row">
-                        <label className="col-sm-2">Name: </label>
-                        <input type="text" className="col-sm-10" onChange={this.onChangeName}/>
-                    </div>
-                    <div className="form-group row">
-                        <label className="col-sm-2">Breed: </label>
-                        <input type="text" className="col-sm-10" onChange={this.onChangeBreed}/>
-                    </div>
-                    <div className="form-group row">
-                        <label className="col-sm-2">Gender: </label>
-                            <div className="form-check form-check-inline">
-                              <input className="form-check-input" type="radio" name="inlineRadioOptions"
-                              id="inlineRadio1" value="Male" onChange={this.onChangeGender} />
-                              <label className="form-check-label" htmlFor="inlineRadio1">Male</label>
-                            </div>
-                            <div className="form-check form-check-inline">
-                              <input className="form-check-input" type="radio" name="inlineRadioOptions"
-                              id="inlineRadio2" value="Female" onChange={this.onChangeGender} />
-                              <label className="form-check-label" htmlFor="inlineRadio2">Female</label>
-                            </div>
-                    </div>
-
-                    <div className="form-group row">
-                        <label className="col-sm-2">Age (years): </label>
-                        <input type="text" className="col-sm-10" onChange={this.onChangeAge}/>
-                    </div>
-                    <div className="form-group row">
-                        <label className="col-sm-2">Vaccinated: </label>
-                        <input className="col-sm-2 mt-2" style={{marginLeft: -90 + 'px'}} type="checkbox" value="" onChange={this.onChangeVaccinated}/>
-                    </div>
-                    <div className="form-group row">
-                        <label className="col-sm-2">Description: </label>
-                        <input type="text" className="col-sm-10" onChange={this.onChangeDescription}/>
-                    </div>
-                    <div className="form-group row">
-                        <label className="col-sm-2">Picture: </label>
-                        <input type="file" className="col-sm-10 form-control-file" name="image" accept="image/*" onChange={this.onChangeImage}/>
-                    </div>
-
-                    {this.state.errors.length>0 && (
-                        <div className="alert alert-danger">
-                            <ul>
-                                {this.state.errors.map(err => <li key={err.msg}>{err.msg}</li>)}
-                            </ul>
+        if (this.state.redirect) {
+            return <Redirect to={'/owners/' + this.props.match.params.owner_id} />;
+        } else {
+            return(
+                <div className="my-4 div-width">
+                    {this.state.formSuccess === true && (
+                        <div className="success-msg mb-3">
+                            <p className="m-0 p-0">Pet Added Successfully!</p>
                         </div>
-                    )}
+                     )}
+                    <div className="mt-2 mb-4"><h2>Add Pet</h2></div>
+                    <form className="pet-details" onSubmit = {this.onSubmit}>
+                        <div className="form-group row">
+                            <label className="col-sm-2">Name: </label>
+                            <input type="text" className="col-sm-10" onChange={this.onChangeName}/>
+                        </div>
+                        <div className="form-group row">
+                            <label className="col-sm-2">Breed: </label>
+                            <input type="text" className="col-sm-10" onChange={this.onChangeBreed}/>
+                        </div>
+                        <div className="form-group row">
+                            <label className="col-sm-2">Gender: </label>
+                                <div className="form-check form-check-inline">
+                                  <input className="form-check-input" type="radio" name="inlineRadioOptions"
+                                  id="inlineRadio1" value="Male" onChange={this.onChangeGender} />
+                                  <label className="form-check-label" htmlFor="inlineRadio1">Male</label>
+                                </div>
+                                <div className="form-check form-check-inline">
+                                  <input className="form-check-input" type="radio" name="inlineRadioOptions"
+                                  id="inlineRadio2" value="Female" onChange={this.onChangeGender} />
+                                  <label className="form-check-label" htmlFor="inlineRadio2">Female</label>
+                                </div>
+                        </div>
 
-                     <div className="form-group row">
-                        <button type="submit" className="btn btn-info ml-3 mt-4">Add Pet</button>
-                    </div>
-                </form>
-            </div>
-        );
+                        <div className="form-group row">
+                            <label className="col-sm-2">Age (years): </label>
+                            <input type="text" className="col-sm-10" onChange={this.onChangeAge}/>
+                        </div>
+                        <div className="form-group row">
+                            <label className="col-sm-2">Vaccinated: </label>
+                            <input className="col-sm-2 mt-2" style={{marginLeft: -90 + 'px'}} type="checkbox" value="" onChange={this.onChangeVaccinated}/>
+                        </div>
+                        <div className="form-group row">
+                            <label className="col-sm-2">Description: </label>
+                            <input type="text" className="col-sm-10" onChange={this.onChangeDescription}/>
+                        </div>
+                        <div className="form-group row">
+                            <label className="col-sm-2">Picture: </label>
+                            <input type="file" className="col-sm-10 form-control-file" name="image" accept="image/*" onChange={this.onChangeImage}/>
+                        </div>
+
+                        {this.state.errors.length>0 && (
+                            <div className="alert alert-danger">
+                                <ul>
+                                    {this.state.errors.map(err => <li key={err.msg}>{err.msg}</li>)}
+                                </ul>
+                            </div>
+                        )}
+
+                         <div className="form-group row">
+                            <button type="submit" className="btn btn-info ml-3 mt-4">Add Pet</button>
+                        </div>
+                    </form>
+                </div>
+            );
+        }
     }
 }
 
